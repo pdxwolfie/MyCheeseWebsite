787c5dbf31d02ed579f0a8733001d8d99935e750
pdxwolfie <s.a.g.wolfie@gmail.com>
2020-06-15 19:25:55 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+print(" Welcome to the totally normal, NOT scethy website to oder your Favoright cheese")
+print (" Are you sure you want to oder here, yes Or no")
+YesOrNO=input()
+
+if YesOrNO == "no" :
+
+ print ("ok well you made the right choice")
+else: 
+ print ("ok well then what do you want?")
+ CheeseType=input()
+ print (" ok is that "+CheeseType+" you odered? ok well any drinks?")
+ DrinkType=input()
+ print ("ok then, want a to know what you odered?")
+ Receipt=input()
+ 
+ if Receipt == "no" :
+   print("ok well it will be at your house in a few minutes.")
+   question=input()
+   if question= "How did you know where my house is"
+   print (...)
+   else:
+     
